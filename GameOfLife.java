253ca9ba216abf9b3242402526123b6dcead23cd
Tiger6849 <cahalll@whs.school.nz>
2026-05-07 11:50:07 +1200

    Adding bluej files
    added bluej files for the first version

---
+++ GameOfLife.java
@@ -0,0 +1,24 @@
+
+/**
+ * GameOf Life is a game based off conways game of life.
+ *
+ * @author Llew cahalane
+ * @version 1 07/05/26
+ */
+import java.awt.event.*;
+import java.util.Scanner;
+public class GameOfLife
+{
+    
+    final int BOARDWIDTH = 20; 
+    final int BOARDHEIGHT = 20;
+    
+    String[][] gameBoard = new String[BOARDWIDTH][BOARDHEIGHT];
+    /**
+     * Constructor for objects of class GameOfLife
+     */
+    public GameOfLife()
+    {
+        
+    }
+}
