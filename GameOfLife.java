66753f3ba022e33d73bf568f5abc0005b49c017b
Tiger6849 <cahalll@whs.school.nz>
2026-05-18 10:37:35 +1200

    added calc code
    added some calc code but it aint working properly

--- GameOfLife.java
+++ GameOfLife.java
@@ -15,6 +15,7 @@ public class GameOfLife
 
     //board array
     String[][] gameBoard = new String[BOARDWIDTH][BOARDHEIGHT];
+    String[][] pastFrame = new String[BOARDWIDTH][BOARDHEIGHT];
 
     //scanner for input
     Scanner keyboard = new Scanner(System.in);
@@ -26,17 +27,21 @@ public class GameOfLife
         //set up
         setUpBoard();
 
-        printBoardAndMenu();
-        if(printQuestionAndCleanInput("Do you want to 1.Move forward some steps or 2.Change the state of a cell?", 1) == "1"){
-            if(printQuestionAndCleanInput("Do you want to 1.Move forward one step or 2.Move forward multiple step?" ,1) == "1"){
-                System.out.println("placeholder for calculating code. move one step forward");
+        while(true){
+            printBoardAndMenu();
+            if(printQuestionAndCleanInput("Do you want to 1.Move forward some steps or 2.Change the state of a cell?", 1).equals("1")){
+                if(printQuestionAndCleanInput("Do you want to 1.Move forward one step or 2.Move forward multiple step?" ,1).equals("1")){
+                    cellCalculation(1);
+                    the goddamn calc scripts aint working!
+                    System.out.println(pastFrame+"banama");
+                }else{
+                    String answer = printQuestionAndCleanInput("How many steps?", 2);
+                    cellCalculation(Integer.parseInt(answer));
+                }
             }else{
-                String answer = printQuestionAndCleanInput("How many steps?", 2);
-                System.out.println("placeholder for calculating code. move "+ answer +" step/s forward");
+                    String answer = printQuestionAndCleanInput("What cell? please answer in x,y format", 3);
+                    changeStateOfCell(answer);
             }
-        }else{
-                String answer = printQuestionAndCleanInput("What cell? please answer in x,y format", 3);
-                System.out.println("placeholder for calculating code. fill in "+ answer);
         }
     }
 
@@ -62,6 +67,7 @@ public class GameOfLife
         String answer = "1";
         int answerX = 1;
         int answerY = 2;
+        String answerXYString = "";
         
         while(!inputSanatized){
             System.out.println(question);
@@ -94,44 +100,12 @@ public class GameOfLife
                     }
                 }
             }else{
-                if(keyboard.hasNextInt()){
-                    answer = keyboard.nextLine();
-                    boolean isAnswerY = false;
-                    String answerXString = "";
-                    String answerYString = "";
-                    boolean fail = false;
-                    
-                    if(answer.length() > 3){
-                            for(int i = 0;i < answer.length();i++){
-                            if(answer.charAt(i) == ','){
-                                isAnswerY = true;
-                            }else if(isAnswerY){
-                                answerYString += answer.charAt(i);
-                            }else if(!isAnswerY){
-                                answerYString += answer.charAt(i);
-                            }else{
-                                System.out.println("Please use format x,y");
-                                fail = true;
-                            }
-                        }
-                        if(!fail){
-                            if(Integer.parseInt(answerYString) <= BOARDHEIGHT && Integer.parseInt(answerYString) >= 0){
-                                answerY = Integer.parseInt(answerYString);
-                                if(Integer.parseInt(answerXString) <= BOARDWIDTH && Integer.parseInt(answerXString) >= 0){
-                                    answerX = Integer.parseInt(answerXString);
-                                    inputSanatized
-                                    you where working on menu option 2 stuff
-                                }else{
-                                    System.out.println("Please say a position on the board");
-                                }
-                            }else{
-                                System.out.println("Please say a position on the board");
-                            } 
-                        }
-                    }else{
-                        System.out.println("Please use format x,y");
-                    }
+                answerXYString = sanatizeCoords(answer,answerX,answerY,inputSanatized);
+                while(answerXYString.equals("retry")){
+                    System.out.println(question);
+                    answerXYString = sanatizeCoords(answer,answerX,answerY,inputSanatized);
                 }
+                inputSanatized = true;
             }
         }
 
@@ -139,7 +113,127 @@ public class GameOfLife
         if(questionType != 3){
             return answer;
         }else{
-            return answerX + ";" + answerY;
+            return answerXYString;
         }
     }
-}
+    
+    public String sanatizeCoords(String answer,int answerX,int answerY,boolean inputSanatized){
+        answer = keyboard.nextLine();
+        boolean isAnswerY = false;
+        String answerXString = "";
+        String answerYString = "";
+        boolean fail = false;
+        
+        if(answer.length() >= 3){
+            for(int i = 0;i < answer.length();i++){
+                if(answer.charAt(i) == ',' && !isAnswerY){
+                    isAnswerY = true;
+                }else if(isAnswerY){
+                    answerYString += answer.charAt(i);
+                }else if(!isAnswerY){
+                    answerXString += answer.charAt(i);
+                }else{
+                    System.out.println("Please use format x,y");
+                    fail = true;
+                }
+            }
+            if(!fail){
+                if(Integer.parseInt(answerYString) < BOARDHEIGHT && Integer.parseInt(answerYString) >= 0){
+                    answerY = Integer.parseInt(answerYString);
+                    if(Integer.parseInt(answerXString) < BOARDWIDTH && Integer.parseInt(answerXString) >= 0){
+                        answerX = Integer.parseInt(answerXString);
+                        inputSanatized = true;
+                    }else{
+                        System.out.println("Please say a position on the board");
+                    }
+                }else{
+                    System.out.println("Please say a position on the board");
+                } 
+            }
+        }else{
+            System.out.println("Please use format x,y");
+        }
+        
+        if(inputSanatized){
+            return answerX + ";" + answerY;
+        }else{
+            return "retry";
+        }
+    }
+    
+    public void cellCalculation(int steps){
+        int liveNeighbours = 0;
+        
+        for(int x=0;x<BOARDWIDTH;x++){
+            for(int y=0;y<BOARDHEIGHT;y++){
+                pastFrame[x][y] = gameBoard[x][y];
+            }
+        }
+        
+        for(int x=0;x<BOARDWIDTH;x++){
+            for(int y=0;y<BOARDHEIGHT;y++){
+                //checks if they are to close to the left edge
+                if(x > 0){
+                    if(pastFrame[x-1][y].equals("#")){liveNeighbours++;};//checks left
+                    if(y > 0)
+                        if(pastFrame[x-1][y-1].equals("#")){liveNeighbours++;};//checks top left
+                    if(y < BOARDHEIGHT-1)
+                        if(pastFrame[x-1][y+1].equals("#")){liveNeighbours++;};//checks bottom left
+                }
+                //checks if they are to close to the top edge
+                if(y > 0){
+                    if(pastFrame[x][y-1].equals("#")){liveNeighbours++;};//checks above
+                    if(x < BOARDWIDTH-1)
+                        if(pastFrame[x+1][y-1].equals("#")){liveNeighbours++;};//checks top right
+                }
+                //checks if they are to close to the right edge
+                if(x < BOARDWIDTH-1){
+                    if(pastFrame[x+1][y].equals("#")){liveNeighbours++;};//checks right
+                    if(y < BOARDHEIGHT-1)
+                        if(pastFrame[x+1][y+1].equals("#")){liveNeighbours++;};//checks bottom right
+                }
+                //checks if they are to close to the bottom edge
+                if(y < BOARDHEIGHT-1)
+                    if(pastFrame[x][y+1].equals("#")){liveNeighbours++;};//checks below
+                
+                if(gameBoard[x][y].equals("#")){
+                    if(liveNeighbours < 2){
+                        gameBoard[x][y] = "="; //underpopulation
+                    }
+                    if(liveNeighbours > 3){
+                        gameBoard[x][y] = "="; //overpopulation
+                    }
+                }else{
+                    if(liveNeighbours == 3){
+                        gameBoard[x][y] = "#"; //reproduction
+                    }   
+                }
+            }
+        }
+    }
+    
+    public void changeStateOfCell(String answer){
+        boolean isAnswerY = false;
+        String answerXString = "";
+        String answerYString = "";
+        
+        for(int i = 0;i < answer.length();i++){
+            if(answer.charAt(i) == ';' && !isAnswerY){
+                isAnswerY = true;
+            }else if(isAnswerY){
+                answerYString += answer.charAt(i);
+            }else if(!isAnswerY){
+                answerXString += answer.charAt(i);
+            }
+        }
+        int answerY = Integer.parseInt(answerYString);
+        int answerX = Integer.parseInt(answerXString);
+            
+        if(gameBoard[answerX][answerY].equals("#")){
+            gameBoard[answerX][answerY] = "=";
+        }else{
+            gameBoard[answerX][answerY] = "#";
+        }
+        }
+    }
+
